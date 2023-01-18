ff5b979eaa3a187234681f942fd120cc99c1e000
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2023-01-18 09:54:58 +0200

    Add files via upload

---
+++ spring/Customer.java
@@ -0,0 +1,27 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customer {
+    @Autowired
+    private DataRequestScope dataRequestScope;
+
+    @Autowired
+    private DataSessionScope dataSessionScope;
+
+    public DataRequestScope getDataRequestScope() {
+        return dataRequestScope;
+    }
+
+    public void setDataRequestScope(DataRequestScope dataRequestScope) {
+        this.dataRequestScope = dataRequestScope;
+    }
+
+    public DataSessionScope getDataSessionScope() {
+        return dataSessionScope;
+    }
+
+    public void setDataSessionScope(DataSessionScope dataSessionScope) {
+        this.dataSessionScope = dataSessionScope;
+    }
+}
